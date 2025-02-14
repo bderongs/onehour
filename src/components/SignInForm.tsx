ae4e5ff4466b8ddcfeade94d6df2985f88e89b58
Matthieu <matthieu@pipplet.com>
2025-02-14 13:37:07 +0100

    Added password suggestion tag

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -76,6 +76,7 @@ export function SignInForm({
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
+                        autoComplete="current-password"
                         className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                         placeholder="Votre mot de passe"
                     />
