cf22eec68d2c8cc3938bf0607ce023d43304f175
Matthieu <matthieu@pipplet.com>
2025-02-18 10:47:49 +0100

    Fixed compile error

--- src/utils/avatar.ts
+++ src/utils/avatar.ts
@@ -1,13 +1,4 @@
 /**
- * Convertit un tableau d'octets en chaîne hexadécimale
- */
-const bytesToHex = (bytes: Uint8Array): string => {
-    return Array.from(bytes)
-        .map(b => b.toString(16).padStart(2, '0'))
-        .join('');
-};
-
-/**
  * Génère une URL d'avatar Dicebear
  * @param id - Identifiant à utiliser comme seed (UUID)
  * @param size - Taille de l'avatar en pixels
